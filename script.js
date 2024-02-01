4c8726702f31e37aea5947d3a506f9dff72d8e81
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-01 16:54:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+let rotateAngle =0;
+
+function rotateLine(){
+	rotateAngle += 2;
+	line.style.transform = `rotate(${rotateAngle}deg)`;
+	requestAnimationFrame(rotateLine);
+}
+rotateLine;
